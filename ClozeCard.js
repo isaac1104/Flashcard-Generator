496453acff284c678233844a4156220d86a17e6b
Isaac Kwon <you@example.com>
2017-08-30 20:09:40 -0700

    3rd Commit: Functioning App

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,10 +1,12 @@
 function ClozeCard(text, cloze) {
-  if (text.includes(cloze)) {
+  if (text.includes(cloze) && this instanceof ClozeCard) {
     this.fullText = text;
     this.cloze = cloze;
     this.partial = text.replace(cloze, "...");
+  } else if (!text.includes(cloze)) {
+    console.log("Oops! " + cloze + " is not in " + text);
   } else {
-    console.log("Oops!" + cloze + " is not in " + text);
+    return new ClozeCard(text, cloze);
   }
 }
 
--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const ClozeCard = require("./ClozeCard");
 
 var flashCard = new BasicCard ("Who was the first president of the United States?", "George Washington");
 
+console.log(flashCard.front);
+console.log(flashCard.back);
+
+var flashCard2 = BasicCard("Who was the first president of the United States?", "George Washington");
+
+console.log(flashCard2.front);
+console.log(flashCard2.back);
+
 var clozeCard = new ClozeCard ("George Washington was the first president of the United States.", "George Washington");
 
-var brokenCloze = new ClozeCard("This doesn't work", "oops");
-
-console.log(clozeCard.cloze);
 console.log(clozeCard.fullText);
+console.log(clozeCard.cloze);
 console.log(clozeCard.partial);
+
+var clozeCard2 = ClozeCard ("George Washington was the first president of the United States.", "George Washington");
+
+console.log(clozeCard2.fullText);
+console.log(clozeCard2.cloze);
+console.log(clozeCard2.partial);
+
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
