2e21706287ef5ffe02b3c1edd6ad68676b198459
Isaac Kwon <you@example.com>
2017-08-30 19:08:01 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const BasicCard = require("./BasicCard");
+
+var flashCard = new BasicCard ("Who was the first president of the United States?", "George Washington");
+
+console.log(flashCard.front);
+console.log(flashCard.back);
